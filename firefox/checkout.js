3b75620a2698b8a37f467deae394515a5b6e4ac7
Peter <gault.peter@gmail.com>
2022-04-05 17:41:24 +0100

    Added no seats check for the venue

--- firefox/checkout.js
+++ firefox/checkout.js
@@ -1,8 +1,11 @@
 if(window.location.href.includes("event")){
-    let findTicketsBtn = document.querySelector("[data-testid='findTicketsBtn']");
-    if(findTicketsBtn){
-        findTicketsBtn.click();
-    }else{
-        location.reload(); 
+    let venueParagraph = document.getElementsByClassName("sc-n00qmo-10 AZZHo")[0]
+    if(venueParagraph.textContent.includes("No map available for this venue.")){
+        let findTicketsBtn = document.querySelector("[data-testid='findTicketsBtn']");
+        if(findTicketsBtn){
+            findTicketsBtn.click();
+        }else{
+            location.reload(); 
+        }
     }
 }
